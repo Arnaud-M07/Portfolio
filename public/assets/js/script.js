aa7752735266c6764501db6d3003eec57712432c
Arnaud-M07 <arnaudmonteux@gmail.com>
2024-01-07 16:04:32 +0100

    JS Animated heading + background fade out

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,29 +3,6 @@
 // // // // // // // // // // // // // //
 
 // Fonction - Masquer le header au scroll
-// function masquerHeaderAuScroll() {
-//     let prevScrollPos = window.scrollY;
-//     let header = document.querySelector('.header-hide');
-//     let scrollThreshold = 300; // Ajuster la distance de déclenchement
-
-//     window.onscroll = function () {
-//         let currentScrollPos = window.scrollY;
-
-//         if (currentScrollPos > scrollThreshold) {
-//             if (prevScrollPos > currentScrollPos) {
-//                 header.classList.remove('header-show');
-//             } else {
-//                 header.classList.add('header-show');
-//             }
-//         } else {
-//             header.classList.remove('header-show');
-//         }
-
-//         prevScrollPos = currentScrollPos;
-//     };
-// }
-
-// Fonction - Masquer le header au scroll
 function masquerHeaderAuScroll() {
     let prevScrollPos = window.scrollY;
     let header = document.querySelector('.header-hide');
@@ -69,4 +46,55 @@ function masquerHeaderAuScroll() {
 //  ??????
 document.addEventListener("DOMContentLoaded", function () {
     masquerHeaderAuScroll();
+});
+
+
+// // // // // // // // // // // // // //
+// // // // // HERO BANNER // // // // //
+// // // // // // // // // // // // // //
+
+// Animated heading 
+// creative / designer / developer
+document.addEventListener("DOMContentLoaded", function () {
+    var animatedHeading1 = document.getElementById('animated-heading-1');
+    var animatedHeading2 = document.getElementById('animated-heading-2');
+    var animatedHeading3 = document.getElementById('animated-heading-3');
+    // Ajoutez une classe pour déclencher l'animation
+    animatedHeading1.classList.add('slide-in');
+    animatedHeading2.classList.add('slide-in');
+    animatedHeading3.classList.add('slide-in');
+});
+// &
+document.addEventListener("DOMContentLoaded", function () {
+    var h1Span = document.querySelector('h1 span');
+    // Ajouter la classe 'visible' après un délai de 3 secondes
+    setTimeout(function () {
+        h1Span.parentElement.classList.add('visible');
+    }, 1300); // Délai de 1300 ms (1.3 secondes)
+});
+
+
+// Background fade out 
+document.addEventListener("DOMContentLoaded", function () {
+    var heroBanner = document.querySelector('.hero-banner');
+    var fadeStart = heroBanner.offsetTop + heroBanner.offsetHeight / 4;
+    var fadeUntil = heroBanner.offsetTop + heroBanner.offsetHeight / 1.5;
+
+    function updateOpacity() {
+        var scroll = window.scrollY;
+        var opacity = 0;
+
+        if (scroll > fadeStart && scroll < fadeUntil) {
+            opacity = (scroll - fadeStart) / (fadeUntil - fadeStart);
+        } else if (scroll >= fadeUntil) {
+            opacity = 1;
+        }
+
+        heroBanner.style.background = `linear-gradient(rgba(20, 20, 20, ${opacity}), rgba(20, 20, 20, ${opacity})), url(/public/assets/img/Hero-banner-bg.jpg)`;
+        heroBanner.style.backgroundSize = 'cover';
+    }
+
+    updateOpacity();
+
+    window.addEventListener('scroll', updateOpacity);
 });
